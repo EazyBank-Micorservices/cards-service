238d090e82826852c9dcee645b44ede02500fb1e
dennis githinji <wakahiad@gmail.com>
2024-01-20 00:25:03 +0300

    setup base entity and card entity

---
+++ src/main/java/org/eazybank/cardsservice/CardsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.eazybank.cardsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eazybank/cardsservice/CardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eazybank.cardsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
