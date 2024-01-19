5f83faa99467cab2eaee834650e0fc7c4614973f
dennis githinji <wakahiad@gmail.com>
2024-01-20 01:11:47 +0300

    add constants

---
+++ src/main/java/org/eazybank/cardsservice/constants/CardConstants.java
@@ -0,0 +1,19 @@
+package org.eazybank.cardsservice.constants;
+
+public final class CardConstants {
+
+    private CardConstants() {
+        // restrict instantiation
+    }
+
+    public static final String  CREDIT_CARD = "Credit Card";
+    public static final int  NEW_CARD_LIMIT = 1_00_000;
+    public static final String  STATUS_201 = "201";
+    public static final String  MESSAGE_201 = "Card created successfully";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+    public static final String  STATUS_400 = "400";
+    public static final String  MESSAGE_400_UPDATE= "Update operation failed. Please try again";
+    public static final String  MESSAGE_400_DELETE= "Delete operation failed. Please try again";
+
+}
